f9bde0cee9384744c43f11ea755f4bdd668cf5ce
ChenCangtao <chencangtao@huawei.com>
2026-05-13 20:24:52 +0800

    【refactor】: refactor sincos to new file
    Co-authored-by: chencangtao<chencangtao@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !2768 merge sincos_refactor into develop
    
    【refactor】: refactor sincos to new file
    
    Created-by: ChenCangtao
    Commit-by: chencangtao
    Merged-by: cann-robot
    Description: 
    
    # Pull Request
    
    ## 描述
    将regbase 自动融合高阶API实现中的三角函数实现抽出来，放入新增的utils文件，供其他API开发使用。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [X] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    重构涉及的高阶API有UT进行功能和精度看护
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [X] 我的代码遵循了项目的代码风格
    - [X] 我已对代码进行了自测
    - [X] 我已更新了相关的文档
    - [X] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [X] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    
    
    See merge request: cann/ge!2768

--- compiler/graph/optimize/autofuse/v35/ascendc/api_regbase/airy_ai.h
+++ compiler/graph/optimize/autofuse/v35/ascendc/api_regbase/airy_ai.h
@@ -9,6 +9,9 @@
  */
 #ifndef __ASCEND_API_AIRY_AI_H
 #define __ASCEND_API_AIRY_AI_H
+
+#include "trigonometric_function_utils.h"
+
 using namespace AscendC;
 
 constexpr float AN[] = {
@@ -133,93 +136,6 @@ constexpr float PI_OVER_4 = 0.7853981633974483f;
 constexpr float AIRY_AI_0 = 0.355028053887817239260;
 constexpr float AIRY_AI_PRIME_0 = -0.258819403792806798405;
 
-constexpr float PI_FOR_X_TODIV = 0.3183098733425140380859375;
-constexpr float PI_0 = 3.140625;
-constexpr float KPI_FIRS_PI_MULS = 0.0009670257568359375;
-constexpr float KPI_TWI_PI_MULS = 6.2771141529083251953125e-7;
-constexpr float KPI_THIR_PI_MULS = 1.21644916362129151821136474609375e-10;
-constexpr float RES_MULTI_SCA = 2.604926501e-6;
-constexpr float RES_ADDICT_UP = -0.0001980894471;
-constexpr float SEC_ADDS = 0.008333049340;
-constexpr float THI_ADDS = -0.1666665792;
-constexpr float POINT_FIVE = 0.5;
-constexpr float M4_SCA = 4.0;
-constexpr float K2_SCA = -2.0;
-constexpr float SCALAR_ONE = 1.0;
-constexpr float COS_KPI_FOR_PI_MULS = -1.0290623200529979163359041220560e-13;
-constexpr float COS_PI_DOWN = 1.57079637050628662109375;
-constexpr float COS_PI_RESDOWN_ADDS_NEG = -0.00000004371139000189375;
-
-template<typename T>
-__simd_callee__ inline void CommonCalInTri(
-    Reg::RegTensor<T>& dstReg, Reg::RegTensor<T>& x, Reg::RegTensor<T>& round,
-    Reg::RegTensor<T>& kpi, Reg::MaskReg mask)
-{
-    Reg::Mul(kpi, x, x, mask);
-    Reg::Muls(dstReg, round, POINT_FIVE, mask);
-    Reg::Truncate<float, RoundMode::CAST_FLOOR, Reg::MaskMergeMode::ZEROING>(dstReg, dstReg, mask);
-    Reg::Muls(dstReg, dstReg, M4_SCA, mask);
-    Reg::Muls(round, round, K2_SCA, mask);
-    Reg::Add(dstReg, dstReg, round, mask);
-    Reg::Adds(dstReg, dstReg, SCALAR_ONE, mask);
-    Reg::Muls(round, kpi, RES_MULTI_SCA, mask);
-    Reg::Adds(round, round, RES_ADDICT_UP, mask);
-    Reg::Mul(round, round, kpi, mask);
-    Reg::Adds(round, round, SEC_ADDS, mask);
-    Reg::Mul(round, round, kpi, mask);
-    Reg::Adds(round, round, THI_ADDS, mask);
-    Reg::Mul(round, round, kpi, mask);
-    Reg::Adds(round, round, SCALAR_ONE, mask);
-    Reg::Mul(round, round, x, mask);
-    Reg::Mul(dstReg, round, dstReg, mask);
-}
-
-template<typename T>
-__simd_callee__ inline void SinForAiryAi(
-    Reg::RegTensor<T>& dstReg, Reg::RegTensor<T>& srcReg, Reg::RegTensor<T>& x,
-    Reg::RegTensor<T>& round, Reg::RegTensor<T>& kpi, Reg::MaskReg mask)
-{
-    Reg::Muls(round, srcReg, PI_FOR_X_TODIV, mask);
-    Reg::Truncate<float, RoundMode::CAST_RINT, Reg::MaskMergeMode::ZEROING>(round, round, mask);
-    Reg::Muls(kpi, round, PI_0, mask);
-    Reg::Sub(x, srcReg, kpi, mask);
-    Reg::Muls(kpi, round, KPI_FIRS_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Muls(kpi, round, KPI_TWI_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Muls(kpi, round, KPI_THIR_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-
-    CommonCalInTri<T>(dstReg, x, round, kpi, mask);
-}
-
-template<typename T>
-__simd_callee__ inline void CosForAiryAi(
-    Reg::RegTensor<T>& dstReg, Reg::RegTensor<T>& srcReg, Reg::RegTensor<T>& x,
-    Reg::RegTensor<T>& round, Reg::RegTensor<T>& kpi, Reg::MaskReg mask)
-{
-    Reg::Muls(round, srcReg, PI_FOR_X_TODIV, mask);
-    Reg::Adds(round, round, POINT_FIVE, mask);
-    Reg::Truncate<float, RoundMode::CAST_RINT, Reg::MaskMergeMode::ZEROING>(round, round, mask);
-    Reg::Muls(kpi, round, PI_0, mask);
-    Reg::Sub(x, srcReg, kpi, mask);
-    Reg::Muls(kpi, round, KPI_FIRS_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Adds(x, x, COS_PI_DOWN, mask);
-    Reg::Muls(kpi, round, KPI_TWI_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Muls(kpi, round, KPI_THIR_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Muls(kpi, round, COS_KPI_FOR_PI_MULS, mask);
-    Reg::Sub(x, x, kpi, mask);
-    Reg::Adds(x, x, COS_PI_RESDOWN_ADDS_NEG, mask);
-
-    CommonCalInTri<T>(dstReg, x, round, kpi, mask);
-
-    Reg::Mins(dstReg, dstReg, SCALAR_ONE, mask);
-    Reg::Maxs(dstReg, dstReg, -SCALAR_ONE, mask);
-}
-
 template<typename T, int32_t iterationNum, int32_t factorLen, const float* factorList>
 __simd_callee__ inline void mainIterForAiryAi(
     Reg::RegTensor<T>& iterReg, Reg::RegTensor<T>& xReg, Reg::MaskReg& mask)
@@ -237,7 +153,6 @@ __simd_callee__ inline void AiryAiNeg(
     Reg::RegTensor<T>& tempRes, Reg::MaskReg& mask)
 {
     Reg::RegTensor<T> z, z2, t, temp, tempX, afn, afd, agn, agd, sinT, cosT;
-    Reg::RegTensor<T> triX, triRound, triKpi;
     Reg::Abs(temp, srcReg, mask);
     Reg::Sqrt(tempX, temp, mask);
     Reg::Mul(temp, temp, tempX, mask); //temp = (-x)^(3/2)
@@ -267,13 +182,13 @@ __simd_callee__ inline void AiryAiNeg(
     Reg::Mul(afn, afn, z2, mask);
     Reg::Adds(afn, afn, (T)1.0, mask);
     
-    SinForAiryAi<T>(sinT, t, triX, triRound, triKpi, mask);
+    AutofuseSin<T>(sinT, t, mask);
     Reg::Mul(sinT, sinT, afn, mask);
 
     Reg::Div(agn, agn, agd, mask);
     Reg::Mul(agn, z, agn, mask);
 
-    CosForAiryAi<T>(cosT, t, triX, triRound, triKpi, mask);
+    AutofuseCos<T>(cosT, t, mask);
     Reg::Mul(cosT, cosT, agn, mask);
 
     Reg::Sub(tempRes, sinT, cosT, mask);
