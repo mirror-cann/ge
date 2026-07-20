34b72af7354b92be3ebc389c57706b2bc687eaad
zhengfeiteng <zhengfeiteng1@h-partners.com>
2026-07-20 21:25:14 +0800

    refactor: 删除ge库上冗余的加头签名脚本。更新cmake仓版本号，解决社区签名报错问题。
    Co-authored-by: Feiteng Zheng<zhengfeiteng1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !4075 merge 20260714-support-build-packages into develop
    
    refactor: 删除ge库上冗余的加头签名脚本。更新cmake仓版本号，解决社区签名报错问题。
    
    Created-by: zhengfeiteng
    Commit-by: Feiteng Zheng
    Merged-by: cann-robot
    Description: 
    # Pull Request
    
    ## 描述
    删除ge库上冗余的加头签名脚本。更新cmake仓版本号，解决社区签名报错问题。
    
    ## 变更类型
    请选择本次引入的变更类型：
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug 修复
    - [ ] ✨ 新功能
    - [ ] 💄 代码风格更新（格式化，局部变量）
    - [x] ♻️ 重构（既不修复错误也不增加功能的代码变动）
    - [ ] 📦 构建过程或辅助工具的变动
    - [ ] 📝 文档内容更新
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在当前页面的右侧'关联Issue'部分添加相应Issue链接，并勾选'合并后关闭已关联的 Issue'选项。 -->
    
    
    ## 如何测试
    描述测试此变更的步骤和前提条件：
    1.对比修改前后ge相关二进制。
    
    ## 核对清单
    <!--  [x] 表示选中 -->
    - [x] 我的代码遵循了项目的代码风格
    - [x] 我已对代码进行了自测
    - [x] 我已更新了相关的文档
    - [x] 我在标题中使用了合适的类型标签（如：`feat:`, `fix:`）
    - [x] 我已经详细阅读了贡献指南（CONTRIBUTING.md），并遵守了其中的所有规定，包括但不限于commit message的格式、无效commit的合并等
    
    ## 其他信息
    在此添加任何其他关于本次 PR 的说明。
    
    
    See merge request: cann/ge!4075

--- cmake/fetch_cann_cmake.cmake
+++ cmake/fetch_cann_cmake.cmake
@@ -13,8 +13,8 @@ if(NOT PROJECT_SOURCE_DIR)
     else()
         include(FetchContent)
 
-        set(CANN_CMAKE_TAG "master-036") # CANN_CMAKE_TAG变量会在scripts/download_third_party_source.sh中用到，确保这个变量存在
-        set(CANN_CMAKE_SHA256 "f8453b8acb2640ab8a0e6fc7dad496091678cbe9e0f7b56b19f8904b4355f25d")
+        set(CANN_CMAKE_TAG "master-042") # CANN_CMAKE_TAG变量会在scripts/download_third_party_source.sh中用到，确保这个变量存在
+        set(CANN_CMAKE_SHA256 "8482467199ba9667b3eb74f38275773944c84c9e5a37ecde1863e5ee4182fe87")
         if(CANN_3RD_LIB_PATH AND EXISTS "${CANN_3RD_LIB_PATH}/cmake-${CANN_CMAKE_TAG}.tar.gz")
             FetchContent_Declare(
                 cann-cmake
